d29fb3e50d40ae8d660257ea191412297e4188cd
Matthew Mulford <mhmulford0@gmail.com>
2022-11-29 16:12:52 -0600

    port config

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,7 @@ fastify.post<{ Body: ResizeBody }>("/resize", {}, async (request, reply) => {
   }
 });
 
-const port = parseInt(process.env.PORT ? process.env.PORT : "") || 3000;
+const port = parseInt(process.env.PORT ? process.env.PORT : "3000");
 
 // Run the server!
 fastify.listen({ port }, async (err) => {
